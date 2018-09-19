bedc0a42dee29c60d4931ad13c8934b1f1cc0210
nadinaaa <nadina_vershinina@mail.ru>
2018-09-20 00:11:26 +0300

    Task is done

--- src/index.js
+++ src/index.js
@@ -2,6 +2,28 @@
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
-module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+module.exports = function getLoveTrianglesCount(preferences=[]) {
+  
+  preferences.unshift(null);
+  pl=preferences.length;
+  var kol=0, k,l,m; 
+  for(var i=1;i<pl;i++)
+  {
+    k=preferences[i];
+    
+    l=preferences[k];
+    
+    if(k==l || k==null|| l==null) continue;
+    m=preferences[l];
+    
+    if(m==i)
+    {
+    kol++;
+    preferences[i]=null;
+    preferences[k]=null;
+    preferences[l]==null;
+    }
+  }
+  return kol;
+ 
 };
